8579457e589cce8373b62121d4049ac82b7af48f
Szeli <szelim@kkszki.hu>
2026-09-17 08:11:21 +0200

    Blogger model is created

---
+++ BlogApi/Models/Blog.cs
@@ -0,0 +1,23 @@
+﻿using Scalar.AspNetCore;
+using System.Runtime.CompilerServices;
+
+namespace BlogApi
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Password { get; set; } = string.Empty;
+        public DateTime RegistrationTime { get; set; }
+    
+public Blog(int Id, string Email, int Age, string Password, DateTime RegistrationTime)
+        {
+            this.Id = Id;
+            this.Email = Email;
+            this.Age = Age;
+            this.Password = Password;
+            this.RegistrationTime = RegistrationTime;
+        }
+    }
+}   
--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b136b7f6a158facc0dabee42fbf3e59d468f84a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

2bcaa42091f57ca8aef02b5fb3eb115089661e92
Szeli <szelim@kkszki.hu>
2026-09-17 08:15:47 +0200

    Updated

--- BlogApi/Models/Blog.cs
+++ BlogApi/Models/Blog.cs
@@ -10,14 +10,6 @@ namespace BlogApi
         public int Age { get; set; }
         public string Password { get; set; } = string.Empty;
         public DateTime RegistrationTime { get; set; }
-    
-public Blog(int Id, string Email, int Age, string Password, DateTime RegistrationTime)
-        {
-            this.Id = Id;
-            this.Email = Email;
-            this.Age = Age;
-            this.Password = Password;
-            this.RegistrationTime = RegistrationTime;
-        }
+
     }
-}   
+}
